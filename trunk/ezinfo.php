f535b5b23101086a2b94f07c4c25446d087a592a
Gaetano Giunta <gg@ez.no>
2012-04-03 10:57:09 +0000

    Add license file, version info file
    git-svn-id: http://svn.projects.ez.no/arh_jdebug@6 09398a38-71bd-49d2-a381-cdba92261cfe

---
+++ trunk/ezinfo.php
@@ -0,0 +1,14 @@
+<?php
+class ggxmlviewInfo
+{
+    static function info()
+    {
+        return array(
+            'Name' => "arh_jdebug",
+            'Version' => "0.3-dev",
+            'Copyright' => "Copyright (C) 2010-2012 A. Rhouati",
+            'License' => "GNU General Public License v2.0"
+        );
+    }
+}
+?>
